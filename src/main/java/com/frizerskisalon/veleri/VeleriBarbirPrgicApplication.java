dc2c9c828e3b6c679e0c3ac6c89c85f4e7e37da7
josko <joskobarbir1988@gmail.com>
2025-03-21 21:50:22 +0100

    osnovne datoteke

---
+++ src/main/java/com/frizerskisalon/veleri/VeleriBarbirPrgicApplication.java
@@ -0,0 +1,13 @@
+package com.frizerskisalon.veleri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeleriBarbirPrgicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeleriBarbirPrgicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frizerskisalon/veleri/VeleriBarbirPrgicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frizerskisalon.veleri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeleriBarbirPrgicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
