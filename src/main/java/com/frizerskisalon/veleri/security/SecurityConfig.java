d05c88c887d37762c2b7472bf5e7719d084c259f
josko <joskobarbir1988@gmail.com>
2025-03-29 01:15:33 +0100

    Izmjenjen SecurityConfig (falile su anotacije)

--- src/main/java/com/frizerskisalon/veleri/security/SecurityConfig.java
+++ src/main/java/com/frizerskisalon/veleri/security/SecurityConfig.java
@@ -1,19 +1,28 @@
 package com.frizerskisalon.veleri.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-public class SecurityConfig{
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated());
-     //   http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        return (SecurityFilterChain)http.build();
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/kontakt").permitAll()
+                .requestMatchers("/admin").denyAll()
+                .anyRequest().authenticated());
+        // http.formLogin(withDefaults());
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+        return http.build();
     }
 }
-
